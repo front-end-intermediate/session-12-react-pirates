83a905a623dca75149d6a2060060748285b08e1c
DannyBoyNYC <DannyBoyNYC>
2017-12-18 15:28:11 -0500

    done

--- react-pirates/src/index.js
+++ react-pirates/src/index.js
@@ -2,33 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import {
-	BrowserRouter as Router,
-	Route
-} from 'react-router-dom';
+// import registerServiceWorker from './registerServiceWorker';
 
-import PirateDetail from './components/PirateDetail';
-
-
-class Main extends React.Component {
-	render() {
-		return (
-			<Router>
-			<div>
-			<Route exact path="/" component={App}/>
-			<Route path="/pirate/:pid" component={PirateDetail} />
-			</div>
-			</Router>
-			)
-	}
-}
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-ReactDOM.render(
-	<Main />,
-	document.getElementById('root')
-	);
-
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
