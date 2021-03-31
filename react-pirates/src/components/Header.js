c0a1546efb3b614329a146885df8d1a537fabb1e
Daniel Deverell <daniel.deverell@gmail.com>
2021-03-31 08:52:11 -0400

    done

--- react-pirates/src/components/Header.js
+++ react-pirates/src/components/Header.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
-import '../assets/css/Header.css'
-import logo from '../assets/img/anchor.svg';
+import React, { Component } from "react";
+import "../assets/css/Header.css";
+import logo from "../assets/img/anchor.svg";
 
 class Header extends React.Component {
-  render(){
+  render() {
     return (
       <div className="header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Header Component</h1>
-      </div>)
-    }
+        <h1>Header Comp</h1>
+      </div>
+    );
   }
+}
 
 export default Header;
